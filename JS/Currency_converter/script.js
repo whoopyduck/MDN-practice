b1c8b2d385498915f1679d2b0a8163503d60ab69
Whoopyduck <if21ansarimohammedmoin@gmail.com>
2026-02-05 19:01:08 +0530

    Created Objects of Currencies and passed down the input value from the input field

--- JS/Currency_converter/script.js
+++ JS/Currency_converter/script.js
@@ -7,18 +7,49 @@ const button1 = document.body.querySelector("#btn1");
 output.value  = inputField.valueAsNumber;
 console.log(typeof inputField.value)
 console.log(typeof inputField.valueAsNumber)
-// console.log(select.value)
+console.log(select.value)
 // test.innerText = "Sup";
 
 
 button1.addEventListener("click" , () => {
     let x = inputField.valueAsNumber;
     output.value = x * 30;
-    console.log(x)
+    // console.log(select.options.length)
+    // console.log(select.length)
+        console.log(currencies.usd.convert(x))
+        for(let i =0 ; i < select.options.length ; i++){
+                console.log(select.value)
+        }
 });
 
-for(let i = 0 ; i < select.options.length ; i++){
+const currencies = { 
+        usd : { 
+            convert(x){
+                let converted = x * 90;
+                return converted;
+            }
+        } , 
 
-    // console.log(select.options[i]);
-    console.log(select)
+        eur : { 
+            convert(x){
+                let converted = x * 106;
+                return converted;
+            }
+            
+        } , 
+
+        cad : { 
+            convert(x){
+                let converted = x * 66;
+                return converted;
+            }
+            
+        } , 
+
+        jpy : { 
+                convert(x){
+                    let converted = x * 0.58;
+                    return converted;
+                }
+        }
 }
