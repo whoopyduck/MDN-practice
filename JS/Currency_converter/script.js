1e81919c051daa3d7fcaba228837a2464503e1de
Whoopyduck <if21ansarimohammedmoin@gmail.com>
2026-02-05 23:20:12 +0530

    Added the logic of conversion and displayed data on UI

--- JS/Currency_converter/script.js
+++ JS/Currency_converter/script.js
@@ -4,7 +4,7 @@ const select = document.body.querySelector("select");
 const test = document.body.querySelector(".op");
 const button1 = document.body.querySelector("#btn1");
 // inputField.setAttribute("value" , 1)
-output.value  = inputField.valueAsNumber;
+
 console.log(typeof inputField.value)
 console.log(typeof inputField.valueAsNumber)
 console.log(select.value)
@@ -13,15 +13,8 @@ console.log(select.value)
 
 button1.addEventListener("click" , () => {
     let x = inputField.valueAsNumber;
-    output.value = x * 30;
-    // console.log(select.options.length)
-    // console.log(select.length)
-        console.log(currencies.usd.convert(x))
-        for(let i =0 ; i < select.options.length ; i++){
-                console.log(select.value)
-        }
+    output.value = `INR ${currencies[select.value].convert(x)}`;
 });
-
 const currencies = { 
         usd : { 
             convert(x){
@@ -48,8 +41,9 @@ const currencies = {
 
         jpy : { 
                 convert(x){
-                    let converted = x * 0.58;
+                    let converted = (x * 0.58).toFixed(2);
                     return converted;
                 }
         }
 }
+output.value  = `INR ${currencies[select.value].convert(inputField.value)}`;
