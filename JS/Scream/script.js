cc7fa089c7efe2cc40240594956f0174d56d6bd6
Whoopyduck <if21ansarimohammedmoin@gmail.com>
2026-02-12 18:37:57 +0530

    Added Experimental Feature as in a dormant stage , Fixed type on button text

--- JS/Scream/script.js
+++ JS/Scream/script.js
@@ -14,13 +14,20 @@ function scream(){
 }
 let clear1 , clear2 ;
 
-btn.addEventListener("mouseover", () => {
- clear1 = setInterval(run, 100)
+btn.addEventListener("mouseover", () => { //Will run two functions with one second and 1 milisecond gap -> setInterval
+ clear1 = setInterval(run, 100)   //Storing seInterval object in a var 
  clear2 = setInterval(scream , 1000)
 });
+
 btn2.addEventListener("mouseover", () => {
-    clearInterval(clear1);
-    clearInterval(clear2);
+    clearInterval(clear1);  //using that var to clear out setInterval
+    clearInterval(clear2);  //by using global function
     document.body.style.backgroundColor = "white";
 })
 
+//Experimental Feature
+/*btn.addEventListener("mouseout", () => {
+    clearInterval(clear1);                    
+    clearInterval(clear2);  
+    document.body.style.backgroundColor = "white";
+}) */
