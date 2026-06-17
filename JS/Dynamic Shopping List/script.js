368c7ca8ddc85d60114283bf82ac549ce1677798
Whoopyduck <if21ansarimohammedmoin@gmail.com>
2026-06-17 20:23:33 +0530

    Can Add Elements and Delete Elements

--- JS/Dynamic Shopping List/script.js
+++ JS/Dynamic Shopping List/script.js
@@ -1,17 +1,33 @@
-const form = document.querySelector("form");
+const button_add = document.querySelector("button");
 const input = document.querySelector("#item");
+const ul = document.querySelector("ul");
 
-form.addEventListener("click" , (e) => {
-    if(e.target.tagName == "BUTTON"){
-        if(input.value == false)
-        {
-            e.preventDefault();
-            alert("Please Enter Some value");
-        }
-        else{
-            e.preventDefault();
-            console.log(`${e.target.tagName} ${input.value}`);
-        }
-    }
-   
-});
+function first(e){
+e.preventDefault();
+if(input.value == false) { //Checking for blank inputs
+    alert("Please Enter some value!");
+}
+else{
+    //Creating li and appending them
+    let inputval = input.value;
+    let li = document.createElement("li");
+    // li.textContent = inputval;
+    let span = document.createElement("span");
+    let button_del = document.createElement("button");
+    span.textContent = inputval;
+    button_del.innerHTML = "Delete";
+    li.appendChild(span);
+    li.appendChild(button_del);
+    button_del.addEventListener("click" , (e) =>{
+        console.log(e.target)
+    })
+    ul.appendChild(li);
+}
+}
+
+button_add.addEventListener("click" , (e) =>
+{
+    first(e);
+}
+);
+
