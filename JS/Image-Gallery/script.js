b9cb4634e82a9150c34871c47e75c905d3b7035b
Whoopyduck <if21ansarimohammedmoin@gmail.com>
2026-06-20 02:08:57 +0530

    Created IMG elements and set their attributes src/alt

--- JS/Image-Gallery/script.js
+++ JS/Image-Gallery/script.js
@@ -3,3 +3,21 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [
+    {filename : "pic1.jpg" , alt : "Closeup of a human eye"} ,
+    {filename : "pic2.jpg" , alt : "Rock that looks like a wave"},
+    {filename : "pic3.jpg" , alt : "Purple and white pansies"},
+    {filename : "pic4.jpg" , alt : "Section of wall from a Pharoah's Tomb"},
+    {filename : "pic5.jpg" , alt : "Large moth on a leaf"}
+];
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (let image of images) {
+    let img = document.createElement("img");
+    // console.log(image)
+    img.setAttribute("src" , `${baseURL}${image["filename"]}`);
+    img.setAttribute("alt" , `${image.alt}`);
+    // console.log(img.getAttribute("alt"))
+}
