b73d5c678d9faca5bdbd91b7cdff3ee9b0c4d3d1
Whoopyduck <if21ansarimohammedmoin@gmail.com>
2026-06-21 21:57:40 +0530

    Completed the remaining part

--- JS/Image-Gallery/script.js
+++ JS/Image-Gallery/script.js
@@ -14,10 +14,47 @@ const images = [
 
 const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
 
+//to iterate over the array of objects
 for (let image of images) {
     let img = document.createElement("img");
     // console.log(image)
     img.setAttribute("src" , `${baseURL}${image["filename"]}`);
     img.setAttribute("alt" , `${image.alt}`);
+    img.setAttribute("tabindex" , 0);
     // console.log(img.getAttribute("alt"))
+
+    //appending imgaes
+    thumbBar.appendChild(img);
+    img.addEventListener("click" , (e) => {
+        updateDisplayedImage(e);
+        // console.log(e.button)
+    });
+    img.addEventListener("keydown" , (e) => {
+        if(e.key === "Enter"){
+            updateDisplayedImage(e); //set image when clicked enter / mimic the first event listener
+        }
+    })
 }
+
+function updateDisplayedImage(e){
+    //get the target event's image attributes 
+    displayedImage.setAttribute("src" , `${e.target.src}`); 
+    displayedImage.setAttribute("alt" , `${e.target.alt}`);
+    // console.log(e.key);
+}
+
+btn.addEventListener("click" , (e) => {
+    if(btn.getAttribute("class" , "dark") === "dark"){
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+        // btn.removeAttribute("class","dark");
+        btn.classList.toggle("dark");
+        // btn.classList.remove("dark");
+    }
+    else{
+        btn.textContent = "Darken";
+        // btn.setAttribute("class" , "dark");
+        btn.classList.toggle("dark");
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0.0)";
+    }
+})
